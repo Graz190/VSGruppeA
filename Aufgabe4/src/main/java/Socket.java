b0d836dfd040bfef87e07bd1e8fade2eed7cfb59
fagherazziC <44192833+fagherazziC@users.noreply.github.com>
2022-05-23 17:12:33 +0200

    Add Dynamic Web Project add Socket class add WebSocketAdapter

---
+++ Aufgabe4/src/main/java/Socket.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+import jakarta.websocket.Session;
+
+public class Socket{
+private List<Session> clients = new ArrayList<>();
+
+@OnOpen
+}
---
+++ Aufgabe4/src/main/java/WebSocketAdapter.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class WebSocketAdapter {
+	public static void main(String[] args) throws IOException {
+		ServerSocket server = new ServerSocket(80);
+	}
+}
