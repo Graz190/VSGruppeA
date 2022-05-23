c151bc0dacacd39fc7335c7c8506b772cb0f0a77
fagherazziC <44192833+fagherazziC@users.noreply.github.com>
2022-05-23 17:30:13 +0200

    add Open/Message/Close

--- Aufgabe4/src/main/java/Socket.java
+++ Aufgabe4/src/main/java/Socket.java
@@ -1,9 +1,27 @@
 import java.util.*;
 
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
 
+@ServerEndpoint("/socket")
 public class Socket{
 private List<Session> clients = new ArrayList<>();
 
 @OnOpen
+public void start(Session session) {
+	clients.add(session);
+}
+@OnMessage
+public void message(Session client, String message) {
+	System.out.println("Client:"+client+" sendete: "+message);
+}
+
+@OnClose
+public void ended(Session session) {
+	clients.remove(session);
+	System.out.println("client "+session+" ist nict mehr dabei");
+}
 }
