1acb187c7ebdc503813b289f06ec09b1464aac42
fagherazziC <44192833+fagherazziC@users.noreply.github.com>
2022-06-10 13:04:58 +0200

    Websocket changes

--- Aufgabe4/src/main/java/WebSocketAdapter.java
+++ Aufgabe4/src/main/java/WebSocketAdapter.java
@@ -1,8 +1,12 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 
+
+
 public class WebSocketAdapter {
 	public static void main(String[] args) throws IOException {
 		ServerSocket server = new ServerSocket(80);
+		Websocket adapter= new Websocket();
+	
 	}
 }
--- Aufgabe4/src/main/java/Websocket.java
+++ Aufgabe4/src/main/java/Websocket.java
@@ -1,27 +1,58 @@
+import java.io.IOException;
 import java.util.*;
 
-import jakarta.websocket.OnClose;
-import jakarta.websocket.OnMessage;
-import jakarta.websocket.OnOpen;
-import jakarta.websocket.Session;
+import jakarta.websocket.*;
 import jakarta.websocket.server.ServerEndpoint;
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 @ServerEndpoint("/socket")
-public class Socket{
-private List<Session> clients = new ArrayList<>();
+public class Websocket {
+	MqttClient test = new MqttClient("ssl://<your_host>:8883", // serverURI in format: 
+            // "protocol://name:port"
+            MqttClient.generateClientId(), // ClientId
+            new MemoryPersistence());
+	private static List<Session> clients = new ArrayList<>();
+	@OnOpen
+	public void start(Session session) {
+		try {
+			synchronized (Websocket.clients) {
+				clients.add(session);
+				session.getBasicRemote().sendText("Verbindung wurde hergestellt.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+	}
 
-@OnOpen
-public void start(Session session) {
-	clients.add(session);
-}
-@OnMessage
-public void message(Session client, String message) {
-	System.out.println("Client:"+client+" sendete: "+message);
-}
+	@OnClose
+	public void ended(Session session) {
+		try {
+			synchronized (Websocket.clients) {
+				clients.remove(session);
+				System.out.println("client " + session + " ist nicht mehr dabei");
+			}
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+	}
 
-@OnClose
-public void ended(Session session) {
-	clients.remove(session);
-	System.out.println("client "+session+" ist nict mehr dabei");
-}
+	@OnMessage
+	public void message(Session session, String message) throws IOException {
+		synchronized (Websocket.clients) {
+			for (Session client : Websocket.clients) {
+				if (client.isOpen() && !client.getId().equals(session.getId()))
+					client.getBasicRemote().sendText("client#" + session.getId() + ": " + message);
+			}
+		}
+	}
+	@OnError
+	public void error(Session session, Throwable ex) throws IOException{
+		synchronized(Websocket.clients) {
+			for(Session client : Websocket.clients) {
+				if(client.isOpen()) 
+					client.getBasicRemote().sendText("client#"+session.getId()+ ": "+ ex.getMessage());
+			}
+		}
+	}
 }
